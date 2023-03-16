d49dbb9cfe7ab9c33501d642d3430b9daa5e3219
extario3231 <ycp0021@gmail.com>
2023-03-16 16:36:41 +0800

    add security config

---
+++ src/main/java/hkmu/comps380f/photoblog/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package hkmu.comps380f.photoblog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filter(HttpSecurity http) throws Exception {
+        return http.httpBasic().disable().build();
+    }
+}
