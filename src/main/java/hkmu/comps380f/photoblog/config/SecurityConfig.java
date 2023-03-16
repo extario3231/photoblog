ec7d84634c43cd247d7f429f276ff6b7d3a2275d
extario3231 <ycp0021@gmail.com>
2023-03-16 19:09:56 +0800

    add security filter chain

--- src/main/java/hkmu/comps380f/photoblog/config/SecurityConfig.java
+++ src/main/java/hkmu/comps380f/photoblog/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package hkmu.comps380f.photoblog.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,6 +12,15 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filter(HttpSecurity http) throws Exception {
-        return http.httpBasic().disable().build();
+        return http.authorizeHttpRequests()
+                .requestMatchers(HttpMethod.GET, "/myprofile").authenticated()
+                .requestMatchers(HttpMethod.GET).permitAll()
+                .requestMatchers(HttpMethod.POST).permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .and()
+                .csrf()
+                .and().build();
     }
 }
