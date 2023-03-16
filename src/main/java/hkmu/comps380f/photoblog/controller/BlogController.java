fbadbb59325d83dab608016c567d92cc7b4ede3b
extario3231 <ycp0021@gmail.com>
2023-03-16 19:08:12 +0800

    move user code to UserController

--- src/main/java/hkmu/comps380f/photoblog/controller/BlogController.java
+++ src/main/java/hkmu/comps380f/photoblog/controller/BlogController.java
@@ -1,15 +1,12 @@
 package hkmu.comps380f.photoblog.controller;
 
 import hkmu.comps380f.photoblog.model.Photo;
-import hkmu.comps380f.photoblog.model.PhotoDto;
-import hkmu.comps380f.photoblog.model.User;
-import hkmu.comps380f.photoblog.model.UserDto;
+import hkmu.comps380f.photoblog.model.dto.PhotoDto;
 import hkmu.comps380f.photoblog.service.PhotoService;
-import hkmu.comps380f.photoblog.service.UserService;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
@@ -22,46 +19,12 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 @Controller
+@RequestMapping("/blog")
 public class BlogController {
     private final PhotoService photoService;
-    private final UserService userService;
 
-    public BlogController(PhotoService photoService, UserService userService) {
+    public BlogController(PhotoService photoService) {
         this.photoService = photoService;
-        this.userService = userService;
-    }
-
-    @GetMapping("/signup")
-    public ModelAndView signup() {
-        return new ModelAndView("signup", "signupForm", new UserDto());
-    }
-
-    @PostMapping("/signup")
-    public View signup(UserDto userDto, HttpSession session) {
-        User newUser = new User();
-        newUser.setName(userDto.getUsername());
-        newUser.setPassword(userDto.getPassword());
-        newUser.setEmail(userDto.getEmail());
-        newUser.setPhoneNumber(userDto.getPhoneNumber());
-        userService.save(newUser);
-
-        session.setAttribute("username", userDto.getUsername());
-
-        return new RedirectView("/", true);
-    }
-
-    @GetMapping("/login")
-    public ModelAndView login() {
-        return new ModelAndView("login", "loginForm", new UserDto());
-    }
-
-    @PostMapping("/login")
-    public View login(UserDto dto, HttpSession session) {
-        User user = userService.findByName(dto.getUsername());
-        if (user == null || !user.getPassword().equals(dto.getPassword()))
-            return new RedirectView("/login", true);
-        session.setAttribute("username", user.getName());
-        return new RedirectView("/", true);
     }
 
     @GetMapping("/upload")
