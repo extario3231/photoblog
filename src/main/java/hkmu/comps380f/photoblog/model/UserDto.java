7860fe38b1a715e26ddd1e5ac4fcdbfefefb2200
extario3231 <ycp0021@gmail.com>
2023-03-16 17:23:55 +0800

    add a user dto

---
+++ src/main/java/hkmu/comps380f/photoblog/model/UserDto.java
@@ -0,0 +1,40 @@
+package hkmu.comps380f.photoblog.model;
+
+public class UserDto {
+    private String username;
+    private String phoneNumber;
+    private String password;
+    private String email;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
