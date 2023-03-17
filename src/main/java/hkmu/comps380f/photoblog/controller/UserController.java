6f87eedf8e3f6ce6715c303e2b33ff9a297d5787
extario3231 <ycp0021@gmail.com>
2023-03-17 17:46:12 +0800

    move some logic from BlogController

--- src/main/java/hkmu/comps380f/photoblog/controller/UserController.java
+++ src/main/java/hkmu/comps380f/photoblog/controller/UserController.java
@@ -4,6 +4,7 @@ import hkmu.comps380f.photoblog.model.User;
 import hkmu.comps380f.photoblog.model.dto.UserDto;
 import hkmu.comps380f.photoblog.service.UserService;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,12 +13,16 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.RedirectView;
 
+import static hkmu.comps380f.photoblog.config.SecurityConfig.getPasswordEncoder;
+
 @Controller
 public class UserController {
     private final UserService userService;
+    private final BCryptPasswordEncoder encoder;
 
     public UserController(UserService userService) {
         this.userService = userService;
+        this.encoder = getPasswordEncoder();
     }
 
     @GetMapping("/signup")
@@ -27,15 +32,8 @@ public class UserController {
 
     @PostMapping("/signup")
     public View signup(UserDto userDto, HttpSession session) {
-        User newUser = new User();
-        newUser.setName(userDto.getUsername());
-        newUser.setPassword(userDto.getPassword());
-        newUser.setEmail(userDto.getEmail());
-        newUser.setPhoneNumber(userDto.getPhoneNumber());
-        userService.save(newUser);
-
+        userService.save(userDto);
         session.setAttribute("username", userDto.getUsername());
-
         return new RedirectView("/", true);
     }
 
@@ -47,10 +45,11 @@ public class UserController {
     @PostMapping("/login")
     public View login(UserDto dto, HttpSession session) {
         User user = userService.findByName(dto.getUsername());
-        if (user == null || !user.getPassword().equals(dto.getPassword()))
+        if (user == null || !user.getPassword().equals(encoder.encode(dto.getPassword())))
             return new RedirectView("/login", true);
         session.setAttribute("username", user.getName());
         session.setAttribute("description", user.getDescription());
+        session.setAttribute("photos", user.getPhotos());
         return new RedirectView("/", true);
     }
 
@@ -58,6 +57,7 @@ public class UserController {
     public String viewProfile(ModelMap modelMap, HttpSession session) {
         modelMap.addAttribute("username", session.getAttribute("username"));
         modelMap.addAttribute("description", session.getAttribute("description"));
+        modelMap.addAttribute("photos", session.getAttribute("photos"));
         return "profile";
     }
 }
