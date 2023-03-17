6109823fcfe7a03f6a896e7a77398a13ade00915
extario3231 <ycp0021@gmail.com>
2023-03-17 17:45:27 +0800

    apply some changes

--- src/main/java/hkmu/comps380f/photoblog/config/SecurityConfig.java
+++ src/main/java/hkmu/comps380f/photoblog/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,8 +13,9 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filter(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests()
-                .requestMatchers(HttpMethod.GET, "/myprofile").authenticated()
+        return http
+                .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.GET, "/myprofile").permitAll()
                 .requestMatchers(HttpMethod.GET).permitAll()
                 .requestMatchers(HttpMethod.POST).permitAll()
                 .and()
@@ -22,5 +24,10 @@ public class SecurityConfig {
                 .and()
                 .csrf()
                 .and().build();
+    }
+
+    @Bean
+    public static BCryptPasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
