e9c2f15abf2dee65a6678896e8231b1152ed50e8
extario3231 <ycp0021@gmail.com>
2023-03-17 14:11:02 +0800

    add Comment dto

---
+++ src/main/java/hkmu/comps380f/photoblog/model/dto/CommentDto.java
@@ -0,0 +1,13 @@
+package hkmu.comps380f.photoblog.model.dto;
+
+public class CommentDto {
+    private String comment;
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
