c78f767efd9956b8e449d2172d0f7cf40ea88093
extario3231 <ycp0021@gmail.com>
2023-03-22 20:23:56 +0800

    modify SQL configurations

--- src/main/java/hkmu/comps380f/photoblog/model/Comment.java
+++ src/main/java/hkmu/comps380f/photoblog/model/Comment.java
@@ -2,13 +2,15 @@ package hkmu.comps380f.photoblog.model;
 
 import jakarta.persistence.*;
 
+import static jakarta.persistence.CascadeType.*;
+
 @Entity
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String comment;
-    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @ManyToOne(cascade = {MERGE, PERSIST, REFRESH, DETACH}, fetch = FetchType.LAZY)
     @JoinColumn(name = "photo.id")
     private Photo photo;
 
