21bd65f599cf223b0b451e8f0a163fc4caafe38e
extario3231 <ycp0021@gmail.com>
2023-03-22 20:29:10 +0800

    remove unnecessary methods

--- src/main/java/hkmu/comps380f/photoblog/repo/UserRepo.java
+++ src/main/java/hkmu/comps380f/photoblog/repo/UserRepo.java
@@ -4,12 +4,11 @@ import hkmu.comps380f.photoblog.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepo extends JpaRepository<User, Long> {
-    List<User> findAll();
     Optional<User> findById(Long id);
-    Optional<User> findByName(String username);
+    Optional<User> findByUsername(String username);
+    void deleteByUsername(String name);
 }
