ffe3e709987118d2f3eacc3b347cfd161ee638df
extario3231 <ycp0021@gmail.com>
2023-03-22 20:24:18 +0800

    remove unnecessary methods

--- src/main/java/hkmu/comps380f/photoblog/repo/CommentRepo.java
+++ src/main/java/hkmu/comps380f/photoblog/repo/CommentRepo.java
@@ -4,9 +4,6 @@ import hkmu.comps380f.photoblog.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface CommentRepo extends JpaRepository<Comment, Long> {
-    List<Comment> findAll();
 }
