9b2d53be3263f4af5b4ec877fd3e427193c1aeca
extario3231 <ycp0021@gmail.com>
2023-03-22 20:31:55 +0800

    change method signatures and add delete method

--- src/main/java/hkmu/comps380f/photoblog/service/UserService.java
+++ src/main/java/hkmu/comps380f/photoblog/service/UserService.java
@@ -1,47 +1,61 @@
 package hkmu.comps380f.photoblog.service;
 
+import hkmu.comps380f.photoblog.exception.UserNotFoundException;
 import hkmu.comps380f.photoblog.model.User;
 import hkmu.comps380f.photoblog.model.UserRole;
 import hkmu.comps380f.photoblog.model.dto.UserDto;
 import hkmu.comps380f.photoblog.repo.UserRepo;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.NoSuchElementException;
+
+import static hkmu.comps380f.photoblog.config.SecurityConfig.passwordEncoder;
 
 @Service
 public class UserService {
     private final UserRepo userRepo;
-//    private final BCryptPasswordEncoder encoder;
+    private final BCryptPasswordEncoder encoder;
 
     public UserService(UserRepo userRepo) {
         this.userRepo = userRepo;
-//        this.encoder = getPasswordEncoder();
+        this.encoder = passwordEncoder();
     }
 
     public List<User> findAll() {
         return userRepo.findAll();
     }
 
-    public User findByName(String name) {
-        return userRepo.findByName(name).orElseThrow(NoSuchElementException::new);
+    public User findByUsername(String name) {
+        return userRepo.findByUsername(name).orElseThrow(UserNotFoundException::new);
     }
 
-    public User findByName(Long id) {
-        return userRepo.findById(id).orElseThrow(NoSuchElementException::new);
+    public User findByUsername(Long id) {
+        return userRepo.findById(id).orElseThrow(UserNotFoundException::new);
     }
 
-    public void saveNewUser(UserDto dto) {
+    public User saveNewUser(UserDto dto) {
         User newUser = new User();
-        newUser.setName(dto.getUsername());
-        newUser.setPassword(dto.getPassword());
+        newUser.setUsername(dto.getUsername());
+        newUser.setPassword(encoder.encode(dto.getPassword()));
         newUser.setEmail(dto.getEmail());
         newUser.setPhoneNumber(dto.getPhoneNumber());
-        newUser.setUserRole(UserRole.USER);
+        newUser.setUserRoles(List.of(UserRole.ADMIN));
         userRepo.save(newUser);
+        return newUser;
     }
 
+    @Transactional(rollbackFor = UserNotFoundException.class)
     public void save(User user) {
         userRepo.save(user);
+    }
+
+    public void deleteByName(String name) {
+        userRepo.deleteByUsername(name);
+    }
+
+    public void deleteById(Long id) {
+        userRepo.deleteById(id);
     }
 }
