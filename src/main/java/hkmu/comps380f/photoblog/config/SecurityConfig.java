df0ecf40f11d6e248df7fedab03e81061cf580f6
extario3231 <ycp0021@gmail.com>
2023-03-22 20:30:15 +0800

    can perform basic authentications

--- src/main/java/hkmu/comps380f/photoblog/config/SecurityConfig.java
+++ src/main/java/hkmu/comps380f/photoblog/config/SecurityConfig.java
@@ -1,36 +1,76 @@
 package hkmu.comps380f.photoblog.config;
 
+import hkmu.comps380f.photoblog.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+
+import static hkmu.comps380f.photoblog.model.UserRole.ADMIN;
+import static hkmu.comps380f.photoblog.model.UserRole.USER;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private final UserService userService;
+
+    public SecurityConfig(UserService userService) {
+        this.userService = userService;
+    }
+
     @Bean
     public SecurityFilterChain filter(HttpSecurity http) throws Exception {
         return http
-//                .authorizeHttpRequests()
-//                .requestMatchers(HttpMethod.GET, "/myprofile").permitAll()
-//                .requestMatchers(HttpMethod.GET).permitAll()
-//                .requestMatchers(HttpMethod.POST).permitAll()
-//                .and()
-//                .formLogin()
-//                .loginPage("/login").permitAll()
-//                .and()
-//                .csrf()
-                .httpBasic()
-                .and().csrf().disable()
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers(HttpMethod.GET, "/profile")
+                        .hasAnyRole(USER.name(), ADMIN.name())
+                        .requestMatchers("/admin").hasRole(ADMIN.name())
+                        .anyRequest().permitAll()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .permitAll()
+                        .successHandler(new AuthSuccessHandler())
+                )
+                .authenticationProvider(authenticationProvider())
+                .csrf().disable()
                 .build();
     }
 
-    // Maybe for later use
-//    @Bean
-//    public static BCryptPasswordEncoder getPasswordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        User.UserBuilder builder = User.builder();
+        List<UserDetails> userDetails = userService.findAll().stream().map(user -> builder
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getUserRoles().stream().map(Enum::name).toArray(String[]::new))
+                .build()
+        ).toList();
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    @Bean
+    public static BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
