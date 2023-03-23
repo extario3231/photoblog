59b8c41bf48d4b74636515d11eaf6a917c131676
extario3231 <ycp0021@gmail.com>
2023-03-23 16:33:06 +0800

    remove not needed import

--- src/main/java/hkmu/comps380f/photoblog/repo/PhotoRepo.java
+++ src/main/java/hkmu/comps380f/photoblog/repo/PhotoRepo.java
@@ -4,8 +4,6 @@ import hkmu.comps380f.photoblog.model.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface PhotoRepo extends JpaRepository<Photo, Long> {
 }
