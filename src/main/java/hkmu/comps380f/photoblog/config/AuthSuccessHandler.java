c12133102aa3bb24252cd0800561eb5cbf444223
extario3231 <ycp0021@gmail.com>
2023-03-31 17:26:27 +0800

    remove unnecessary code

--- src/main/java/hkmu/comps380f/photoblog/config/AuthSuccessHandler.java
+++ src/main/java/hkmu/comps380f/photoblog/config/AuthSuccessHandler.java
@@ -4,7 +4,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -15,8 +14,6 @@ public class AuthSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         if (authentication.isAuthenticated()) {
-            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-            request.getSession().setAttribute("username", userDetails.getUsername());
             response.sendRedirect("/");
         }
     }
