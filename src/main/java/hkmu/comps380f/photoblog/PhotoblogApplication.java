ddda95c6a53ac0d0cb97bb357e784dd3642a8ced
extario3231 <ycp0021@gmail.com>
2023-04-07 11:33:02 +0800

    remove database init code

--- src/main/java/hkmu/comps380f/photoblog/PhotoblogApplication.java
+++ src/main/java/hkmu/comps380f/photoblog/PhotoblogApplication.java
@@ -1,12 +1,9 @@
 package hkmu.comps380f.photoblog;
 
-import hkmu.comps380f.photoblog.model.dto.UserDto;
-import hkmu.comps380f.photoblog.service.UserService;
 import org.apache.catalina.Context;
 import org.apache.tomcat.util.descriptor.web.JspConfigDescriptorImpl;
 import org.apache.tomcat.util.descriptor.web.JspPropertyGroup;
 import org.apache.tomcat.util.descriptor.web.JspPropertyGroupDescriptorImpl;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
@@ -14,25 +11,12 @@ import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerF
 import org.springframework.context.annotation.Bean;
 
 import java.util.Collections;
-import java.util.List;
-
-import static hkmu.comps380f.photoblog.model.UserRole.ADMIN;
-import static hkmu.comps380f.photoblog.model.UserRole.USER;
 
 @SpringBootApplication
 public class PhotoblogApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PhotoblogApplication.class, args);
-    }
-
-    @Bean
-    CommandLineRunner commandLineRunner(UserService userService) {
-        return args -> {
-            userService.saveNewUser(new UserDto("chris", "chris", "hi", "75639562", "chris@hkmu.edu", List.of(USER)));
-            userService.saveNewUser(new UserDto("sarah", "sarah", "hi", "65479853", "sarah@hkmu.edu", List.of(USER)));
-            userService.saveNewUser(new UserDto("max", "max", "hi", "95213001", "max@hkmu.edu", List.of(USER, ADMIN)));
-        };
     }
 
     @Bean
