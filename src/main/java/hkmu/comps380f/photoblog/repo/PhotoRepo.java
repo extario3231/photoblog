0cee4775383c685bf2a61dbe03162743153b2fa4
extario3231 <ycp0021@gmail.com>
2023-04-11 16:05:24 +0800

    add method for finding photos by uploader

--- src/main/java/hkmu/comps380f/photoblog/repo/PhotoRepo.java
+++ src/main/java/hkmu/comps380f/photoblog/repo/PhotoRepo.java
@@ -3,5 +3,8 @@ package hkmu.comps380f.photoblog.repo;
 import hkmu.comps380f.photoblog.model.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PhotoRepo extends JpaRepository<Photo, Long> {
+    List<Photo> findAllByUploader(String uploader);
 }
