5dfaaf2b18224cb15e22d82bc2e21ba5ae33e119
extario3231 <ycp0021@gmail.com>
2023-04-14 12:50:40 +0800

    find comments by photo id

--- src/main/java/hkmu/comps380f/photoblog/repo/CommentRepo.java
+++ src/main/java/hkmu/comps380f/photoblog/repo/CommentRepo.java
@@ -3,5 +3,8 @@ package hkmu.comps380f.photoblog.repo;
 import hkmu.comps380f.photoblog.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepo extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByPhotoId(Long photoId);
 }
